6c784dde133ff35d909ab8840a7a92067dd27e04
francivaldo4334 <francivaldodev@gmail.com>
2025-08-08 09:29:48 -0300

    :construction: adiciona metadatas e o campo opcional de valor residual

--- assetscontrol/src/main/java/br/com/assetscontrol/app/model/Asset.java
+++ assetscontrol/src/main/java/br/com/assetscontrol/app/model/Asset.java
@@ -2,11 +2,32 @@ package br.com.assetscontrol.app.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "asset")
 class Asset {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @Column(nullable = false)
   private String name;
+  @ManyToOne
+  @JoinColumn(name = "asset_type_id", nullable = false)
   private AssetType assetType;
+  @Column(name = " purchase_price_cents", nullable = false)
   private Integer purchasePriceCents;
+  @Column(name = "purchase_date", nullable = false)
   private Date purchaseDate;
+  @Column(name = "use_full_live_years", nullable = false)
   private Integer useFullLiveYears;
+  @Column(name = "residual_value_cents")
+  private Integer residualValueCents;
 }
--- assetscontrol/src/main/java/br/com/assetscontrol/app/model/AssetType.java
+++ assetscontrol/src/main/java/br/com/assetscontrol/app/model/AssetType.java
@@ -1,6 +1,18 @@
 package br.com.assetscontrol.app.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "asset_type")
 class AssetType {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @Column(nullable = false)
   private String name;
 }
